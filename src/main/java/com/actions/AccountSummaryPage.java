e406fb2bbcb19bd47973801db3216048b63c59bd
Your Name <SaurabhM84@Saurabhs-MacBook-Pro.local>
2019-11-30 14:32:19 -0500

    firstcommit

---
+++ src/main/java/com/actions/AccountSummaryPage.java
@@ -0,0 +1,46 @@
+package com.actions;
+
+
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
+import com.base.Page;
+import com.locators.AccountSummaryPageLocators;
+
+public class AccountSummaryPage extends Page {
+	
+	public AccountSummaryPageLocators acctsummary;
+	public AccountSummaryPage() {
+		this.acctsummary=new AccountSummaryPageLocators();
+		AjaxElementLocatorFactory factory = new AjaxElementLocatorFactory(driver, 10);
+		PageFactory.initElements(factory, this.acctsummary);
+	}
+	
+	public String actual = driver.getTitle();
+
+	
+	public void goToAccountActivity() {
+		
+		
+	}
+	
+	public void goToTransferFund() {
+		
+	}
+	
+	public void goToPayBills() {
+		acctsummary.paybillsbtn.click();
+		
+	}
+	
+	public void goToMyMoneyMap() {
+		
+		
+	}
+	
+	public void goToOnlineStatements() {
+		
+		
+	}
+
+}
---
+++ src/test/java/com/testcases/SignInTest.java
@@ -0,0 +1,25 @@
+package com.testcases;
+
+import org.testng.annotations.Test;
+
+import com.actions.HomePage;
+import com.base.Page;
+
+
+public class SignInTest extends Page {
+
+
+	@Test
+	public void signInTest() {
+
+		Page.initConfiguration();
+		//test.log(LogStatus.INFO, "SignIn Test started");
+		HomePage home=new HomePage();
+		home.gotoSignIn();
+		//test.log(LogStatus.INFO, "SignIn Test ended");
+		Page.quitBrowser();	
+
+	}
+
+}
+  
